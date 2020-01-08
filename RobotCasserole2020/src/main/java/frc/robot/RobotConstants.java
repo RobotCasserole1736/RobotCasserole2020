9f3aa3eb83831b80175f0fe9f4df76af1d54dda9
Chris Gerth <chrisgerth010592@gmail.com>
2020-01-08 17:54:06 -0600

    Added a few new skeleton classes

---
+++ RobotCasserole2020/src/main/java/frc/robot/RobotConstants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class RobotConstants{
+
+
+    //This class should contain a bunch of static-public-final constant values
+    // which other classes can reference. Use it for things that would otherwise
+    // be hardcoded, to give your values meaningful names.
+
+    static public final int DT_LEFT_NEO_1_CANID = 1;
+    static public final int DT_LEFT_NEO_2_CANID = 2;
+    static public final int DT_RIGHT_NEO_1_CANID = 3;
+    static public final int DT_RIGHT_NEO_2_CANID = 4;
+
+}
