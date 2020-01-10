be853c624695c6f6928264c02e4433c526fd4a9b
ADA <adacasserole@hotmail.com>
2020-01-09 19:35:25 -0600

    still working

--- RobotCasserole2020/src/main/java/frc/robot/Drivetrain/RealDrivetrain.java
+++ RobotCasserole2020/src/main/java/frc/robot/Drivetrain/RealDrivetrain.java
@@ -1,49 +1,104 @@
 package frc.robot.Drivetrain;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
+import edu.wpi.first.hal.sim.mockdata.PDPDataJNI;
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import frc.robot.RobotConstants;
 
 public class RealDrivetrain extends Drivetrain {
 
-    CANSparkMax DtLeftMaster;
-    CANSparkMax DtRightMaster;
-    
-    CANSparkMax DtLeftIntern;
-    CANSparkMax DtRightIntern;
+    //TODO Put in Robot Constants
 
-    CasseroleGyro gyro;
+
+    
+    CANSparkMax dtLeftMaster;
+    CANSparkMax dtRightMaster;
+    CANEncoder leftEncoder;
+
+    CANSparkMax dtLeftIntern;
+    CANSparkMax dtRightIntern;
+    CANEncoder rightEncoder;
+
+    Gyro dtGyro;
+    
+
+    
+    //State Data
+    double fwdRevCmd = 0;
+    double rotCmd = 0;
+
+    DrivetrainOpMode opMode; /* The present operational mode */
+    DrivetrainOpMode opModeCmd; /* The most recently commanded operational mode */
+    DrivetrainOpMode prevOpMode; /* the previous operational mode */
+    
+    //Sensor Data
+    
+    double dtNeoL1Current = 0;
+    double dtNeoL2Current = 0;
+    double dtNeoR1Current = 0;
+    double dtNeoR2Current = 0;
+    double leftWheelSpeedRPM = 0;
+    double rightWheelSpeedRPM = 0;
+    double gyroAngle = 0;
+    double gyroLockRotationCmd = 0;
+
+
+    
 
     public RealDrivetrain(){
-    
-        DrivetrainOpMode opMode; /* The present operational mode */
-        DrivetrainOpMode opModeCmd; /* The most recently commanded operational mode */
-        DrivetrainOpMode prevOpMode; /* the previous operational mode */
-    
-        gyro = new CasseroleGyro();
+        dtLeftIntern = new CANSparkMax(RobotConstants.DT_LEFT_NEO_1_CANID, MotorType.kBrushless);
+        dtLeftIntern = new CANSparkMax(RobotConstants.DT_LEFT_NEO_2_CANID, MotorType.kBrushless);
+        dtLeftIntern = new CANSparkMax(RobotConstants.DT_RIGHT_NEO_2_CANID, MotorType.kBrushless);
+        dtLeftIntern = new CANSparkMax(RobotConstants.DT_RIGHT_NEO_2_CANID, MotorType.kBrushless);
+        
+
+
+        for(int i =0; i < 10; i++){
+            dtLeftIntern.follow(dtLeftMaster);
+            dtRightIntern.follow(dtRightMaster);
+        }
+        
 
     }
 
+   
     @Override
     public void update() {
         // TODO Auto-generated method stub
-        gyro.update();
+        //sensors
+        dtNeoL1Current = dtLeftMaster.getOutputCurrent();
+        dtNeoL2Current = dtLeftIntern.getOutputCurrent();
+        dtNeoR1Current = dtRightMaster.getOutputCurrent();
+        dtNeoR2Current = dtRightIntern.getOutputCurrent();
+
+        leftWheelSpeedRPM = dtLeftMaster.getEncoder().getVelocity();
+        rightWheelSpeedRPM = dtRightMaster.getEncoder().getVelocity();
+
+
+
+        
         
     }
-
     @Override
     public void setOpenLoopCmd(double forwardReverseCmd, double rotaionCmd) {
-        // TODO Auto-generated method stub
+        opModeCmd = DrivetrainOpMode.kOpenLoop;
+        forwardReverseCmd = fwdRevCmd;
 
     }
 
     @Override
     public void setGyroLockCmd(double forwardReverseCmd) {
-        // TODO Auto-generated method stub
+        opModeCmd = DrivetrainOpMode.kGyroLock;
+        forwardReverseCmd = fwdRevCmd;
 
     }
 
     @Override
     public void setPositionCmd(double forwardReverseCmd, double angleError) {
-        // TODO Auto-generated method stub
+        opModeCmd = DrivetrainOpMode.kTargetPosition;
 
     }
 
@@ -86,31 +141,33 @@ public class RealDrivetrain extends Drivetrain {
     @Override
     public double getGyroAngle() {
         // TODO Auto-generated method stub
-        return 0;
+        return gyroAngle;
     }
 
     @Override
     public double getLeftNeo1Current() {
-        // TODO Auto-generated method stub
-        return 0;
+        
+        return dtNeoL1Current;
     }
 
     @Override
     public double getLeftNeo2Current() {
         // TODO Auto-generated method stub
-        return 0;
+        return dtNeoL2Current;
     }
 
     @Override
     public double getRightNeo1Current() {
         // TODO Auto-generated method stub
-        return 0;
+        return dtNeoR1Current;
     }
 
     @Override
     public double getRightNeo2Current() {
         // TODO Auto-generated method stub
-        return 0;
+        return dtNeoR2Current;
     }
-
+    public double getGyroLockRotationCmd(){
+        return gyroLockRotationCmd;
+    }
 }
