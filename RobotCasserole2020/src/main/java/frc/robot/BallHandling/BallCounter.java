3fdcadf2bd6f9ea628107b01c160bdc9743e9373
robert3827 <holemanr3@gmail.com>
2020-01-28 18:36:43 -0600

    some logic that may be correct

--- RobotCasserole2020/src/main/java/frc/robot/BallHandling/BallCounter.java
+++ RobotCasserole2020/src/main/java/frc/robot/BallHandling/BallCounter.java
@@ -7,8 +7,106 @@
 
 package frc.robot.BallHandling;
 
-/**
- * Add your docs here.
- */
-public class BallCounting {
+import org.eclipse.jetty.util.HttpCookieStore.Empty;
+
+import frc.lib.Calibration.Calibration;
+import frc.robot.BallHandling.Conveyor.ConveyerOpMode;
+import frc.lib.DataServer.Signal;
+
+
+
+
+public class BallCounter {
+
+    //State Data
+    public enum BallHeight {
+        Empty(0),
+        ShortButPresent(1),
+        Apex(2);
+
+        public final int value;
+
+        private BallHeight(int value) {
+            this.value = value;
+        }
+    }
+
+    //State Data    
+    int ballsInConveyor = 0;
+    BallHeight curBallHeight;
+    BallHeight prevBallHeight;
+
+    Double ballHeightIn;
+
+
+    //Calibrations
+    Calibration ballMinThicknessCal;
+    Calibration ballMaxThicknessCal;
+    
+    //Signals
+    Signal ballCountSig;
+    Signal howFarFromSensorSig;
+    Signal whatHeightWeSaySig;
+
+    private static BallCounter inst = null;
+    public static synchronized BallCounter getInstance() {
+        if (inst == null)
+            inst = new BallCounter();
+        return inst;
+
+    }
+    
+    private BallCounter() {
+        ballMinThicknessCal = new Calibration("Ball Is too Short", 1);
+        ballMaxThicknessCal = new Calibration("Thick Side of Ball", 5);
+        
+        ballCountSig = new Signal("How many balls are in the Conveyor", "balls");
+        howFarFromSensorSig = new Signal("Distance From Ball to Sensor", "inches");
+        whatHeightWeSaySig = new Signal("What Are We Calling The Measurement", "NameOfState");
+
+    }
+    public void update() {
+        
+        if(curBallHeight != prevBallHeight) {
+            if(curBallHeight == BallHeight.Apex) {
+                ConveyerOpMode checkVal=Conveyor.getInstance().getOpMode();
+                if(checkVal == ConveyerOpMode.AdvanceFromHopper || checkVal == ConveyerOpMode.AdvanceToShooter || checkVal == ConveyerOpMode.InjectIntoShooter) {
+                    ballsInConveyor +=1 ;
+                }else if(checkVal == ConveyerOpMode.Reverse) {
+                    ballsInConveyor -= 1;
+                }
+
+            }
+        }
+    }
+    public void setBallHeight() {
+        ballHeightIn = BallDistanceSensor.getInstance().getDistance_in();
+        if(ballHeightIn > ballMaxThicknessCal.get()) {
+            curBallHeight = BallHeight.Apex;
+        } else if (ballHeightIn < ballMaxThicknessCal.get() && ballHeightIn > ballMinThicknessCal.get()) {
+            curBallHeight = BallHeight.ShortButPresent;
+        }else {
+            curBallHeight = BallHeight.Empty;
+        }
+
+
+    }
+    public int getBallCount() {
+        
+        return ballsInConveyor;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
