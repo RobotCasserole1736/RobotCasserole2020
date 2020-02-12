c6b457c4411b7988a352b748131887d47d060e7d
AdenThomas <adenharriman@gmail.com>
2020-02-11 20:00:18 -0600

    forgot that a variable declaration changed. It's fixed now.

--- RobotCasserole2020/src/main/java/frc/robot/Robot.java
+++ RobotCasserole2020/src/main/java/frc/robot/Robot.java
@@ -448,8 +448,8 @@ public class Robot extends TimedRobot {
     CasseroleDriverView.setBoolean("Vision Target Visible", cam.isTgtVisible());
     CasseroleDriverView.setBoolean("Climber Lower SW Fault", climber.isLowerLimitSwitchFaulted());
     CasseroleDriverView.setBoolean("Climber Upper SW Fault", climber.isUpperLimitSwitchFaulted());
-    CasseroleDriverView.setBoolean("Climber Upper Limit Switch Pressed", climberUpperLSPressed);
-    CasseroleDriverView.setBoolean("Climber Lower Limit Switch Pressed", climberLowerLSPressed);
+    CasseroleDriverView.setBoolean("Climber Upper Limit Switch Pressed", climber.climberUpperLSPressed);
+    CasseroleDriverView.setBoolean("Climber Lower Limit Switch Pressed", climber.climberLowerLSPressed);
     CasseroleDriverView.setBoolean("Pnuematic Pressure", pneumaticPressureLow);
     CasseroleDriverView.setBoolean("Conveyor Full", conveyorFull);
     CasseroleDriverView.setBoolean("Shooter Spoolup", (shooterCtrl.getShooterCtrlMode() == ShooterCtrlMode.SpoolUp));
