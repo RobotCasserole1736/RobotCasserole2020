c24d4cdb81f74778175fdf118410500fd23f3d16
Chris Gerth <chrisgerth010592@gmail.com>
2020-02-17 21:16:40 -0600

    Added new indicator for vision angle

--- RobotCasserole2020/src/main/java/frc/robot/Robot.java
+++ RobotCasserole2020/src/main/java/frc/robot/Robot.java
@@ -438,7 +438,8 @@ public class Robot extends TimedRobot {
     CasseroleDriverView.newDial("System Press (PSI)", 0, 150, 10, 90, 130);
     CasseroleDriverView.newDial("Shooter Speed (RPM)", 0, 6000, 600, 4500, 5700);
     CasseroleDriverView.newDial("Robot Speed (fps)", 0, 20, 2, 5, 15);
-    CasseroleDriverView.newDial("Robot Angle (degrees)", -180, 180, 45, -10, 10);
+    CasseroleDriverView.newDial("Robot Angle (deg)", -180, 180, 45, -10, 10);
+    CasseroleDriverView.newDial("Vision Tgt Angle (deg)", -30, 30, 5, -2.5, 2.5);
     CasseroleDriverView.newBoolean("Vision Camera Fault", "red");
     CasseroleDriverView.newBoolean("Vision Target Visible", "green");
     CasseroleDriverView.newBoolean("Climber Lower SW Fault", "red");
@@ -462,6 +463,7 @@ public class Robot extends TimedRobot {
     CasseroleDriverView.setDialValue("Shooter Speed (RPM)", shooterCtrl.getSpeedRPM());
     CasseroleDriverView.setDialValue("Robot Speed (fps)", drivetrain.getRobotSpeedfps());
     CasseroleDriverView.setDialValue("Robot Angle (degrees)", robotTilt.getRobotAngle());
+    CasseroleDriverView.setDialValue("Vision Tgt Angle (deg)", cam.isTgtVisible()?cam.getTgtGeneralAngle():-50);
     CasseroleDriverView.setBoolean("Vision Camera Fault", !cam.isVisionOnline());
     CasseroleDriverView.setBoolean("Vision Target Visible", cam.isTgtVisible());
     CasseroleDriverView.setBoolean("Climber Lower SW Fault", climber.isLowerLimitSwitchFaulted());
