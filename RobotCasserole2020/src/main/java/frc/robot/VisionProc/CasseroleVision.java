34a4b469d0457cc2dd07005adf3d4b0cfdbb0012
Chris <chrisgerth010592@gmail.com>
2021-10-01 17:59:50 -0500

    WIP moving robot to photonvision.... but can't instasll vendordep yet?

--- RobotCasserole2020/src/main/java/frc/robot/VisionProc/CasseroleVision.java
+++ RobotCasserole2020/src/main/java/frc/robot/VisionProc/CasseroleVision.java
@@ -9,6 +9,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import frc.lib.DataServer.Signal;
 import frc.robot.LoopTiming;
 
@@ -22,22 +23,7 @@ public class CasseroleVision {
         return instance;
     }
 
-    NetworkTableEntry proc_duration_sec_nt;
-    NetworkTableEntry framerate_fps_nt;
-    NetworkTableEntry targetVisible_nt;
-    NetworkTableEntry targetAngle_deg_nt;
-    NetworkTableEntry targetPosStable_nt;
-    NetworkTableEntry FuzzyPickles;
-    NetworkTableEntry InMatch;
-    NetworkTableEntry MatchNumber;
-    NetworkTableEntry InnerAim;
-    NetworkTableEntry Heartbeat;
 
-    double camHbeatVal = 0;
-    double camHbeatPrev = 0;
-    final int CAM_HBEAT_LIMIT_LOOPS = 1500; //3 second timeout
-    int camHbeatCounter = CAM_HBEAT_LIMIT_LOOPS;
-    
     double proc_duration_sec;
     double framerate_fps;
     double targetAngle_deg;
@@ -55,25 +41,11 @@ public class CasseroleVision {
     Signal cameraDurationSignal;
     Signal visionOnlineSignal;
 
-
+    PhotonCamera camera;
 
     private CasseroleVision(){
         NetworkTableInstance.getDefault().setUpdateRate(0.01); //SPEEEEEEEEEEED
         NetworkTable table = NetworkTableInstance.getDefault().getTable("VisionData");
-
-        //Camera -> RIO
-        proc_duration_sec_nt = table.getEntry("proc_duration_sec");
-        framerate_fps_nt = table.getEntry("framerate_fps");
-        targetVisible_nt = table.getEntry("targetVisible");
-        targetAngle_deg_nt = table.getEntry("targetAngle_deg");
-        targetPosStable_nt = table.getEntry("targetPosStable");
-
-        //RIO -> Camera
-        FuzzyPickles = table.getEntry("Fuzzy Pickles");
-        InMatch = table.getEntry("InMatch");
-        MatchNumber = table.getEntry("MatchNumber");
-        InnerAim = table.getEntry("InnerAim");
-        Heartbeat = table.getEntry("Heartbeat");
 
         targetAngleSignal= new Signal("Vision Raspberry Pi Angle","deg");
         targetVisibleSignal= new Signal("Vision Raspberry Pi Visible Target","bool");
@@ -82,51 +54,34 @@ public class CasseroleVision {
         cameraDurationSignal= new Signal("Vision Raspberry Pi Duration","sec");
         visionOnlineSignal= new Signal("Vision Raspberry Pi Vision System Online","bool");
 
-        updateMatchData();
+        camera = new PhotonCamera("photonvision");
     }
-
-    private String getDateTimeString() {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        df.setTimeZone(TimeZone.getTimeZone("US/Central"));
-        return df.format(new Date());
-    }
-
-    private void updateMatchData(){
-        InMatch.setBoolean(true);
-        MatchNumber.setString(DriverStation.getInstance().getEventName()+"_"
-        +DriverStation.getInstance().getMatchType()+"_"
-        +Integer.toString(DriverStation.getInstance().getMatchNumber())+"_"
-        +  getDateTimeString());
-    }
-
 
     public void update() {
         //read values periodically
         double sampleTimeMs = LoopTiming.getInstance().getLoopStartTimeSec()*1000.0;
-        proc_duration_sec = proc_duration_sec_nt.getDouble(-1.0);
-        framerate_fps = framerate_fps_nt.getDouble(-1.0);
-        targetVisible = convertDoubletoBoolean(targetVisible_nt.getDouble(0.0));
-        targetPosStable = convertDoubletoBoolean(targetPosStable_nt.getDouble(0.0));
-        targetAngle_deg = targetAngle_deg_nt.getDouble(-1.0);
-        visionUpdatedTime = targetAngle_deg_nt.getLastChange();
-        
-        camHbeatPrev = camHbeatVal;
-        camHbeatVal = Heartbeat.getDouble(0.0);
 
-        //Expect the vision process software to update a heartbeat periodically.
-        // Declare vision offline if that heartbeat gets stuck.
-        if(camHbeatPrev != camHbeatVal){
-            camHbeatCounter = CAM_HBEAT_LIMIT_LOOPS;
-            visionOnline = true;
+        var result = camera.getLatestResult();
+
+        proc_duration_sec = result.getLatencyMillis() / 1000.0;
+        framerate_fps = -1.0;
+        targetVisible = result.hasTargets();
+        visionUpdatedTime = Timer.getFPGATimestamp() - result.getLatencyMillis() / 1000.0;
+
+
+        if(targetVisible){
+            var target = result.getBestTarget();
+            targetPosStable = true;
+            targetAngle_deg = target.getYaw();
         } else {
-            if(camHbeatCounter > 0){
-                camHbeatCounter--;
-                visionOnline = true;
-            } else {
-                visionOnline = false;
-                targetVisible = false;
-            }
+            targetPosStable = false;
+            targetAngle_deg = 0.0;
         }
+
+        
+
+        visionOnline = true;
+
 
         targetAngleSignal.addSample(sampleTimeMs, targetAngle_deg);
         targetVisibleSignal.addSample(sampleTimeMs, targetVisible);
@@ -166,11 +121,12 @@ public class CasseroleVision {
     }
 
     public void TakeAPicture(){
-        FuzzyPickles.setBoolean(true);
+        camera.takeInputSnapshot();
+        camera.takeOutputSnapshot();
     }
 
     public void setInnerGoalAsTarget(boolean input){
-        InnerAim.setBoolean(input);
+        //TBD.... nothing I guess?
     }
 
 }
